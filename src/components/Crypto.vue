4be31e1f14dd8c7aa6dae84b6e87a03ef468f22b
Nihir Shah <nihir10dec@gmail.com>
2020-12-30 22:41:00 +0530

    added green and red color to changes

--- src/components/Crypto.vue
+++ src/components/Crypto.vue
@@ -22,8 +22,17 @@
   <template v-slot:[`item.icon`]="{ item }">
     <img :src="item.icon" style="width: 30px; height: 30px" />    
   </template>
+  <!-- <template v-slot:[`item.price_change`]="{ item }">
+    <span style="width: 30px; height: 30px ; color:'green' "> {{item.price_change}} </span>
+  </template> -->
   
-  
+  <template v-slot:[`item.price_change`]="{ item }">
+      <span v-bind:class="getColor(item.price_change)" >
+        <span v-if="item.price_change > 0"> + </span>
+        {{ item.price_change }}
+      </span>
+    </template>
+
   </v-data-table>
 </v-card>
 </template>
@@ -49,6 +58,22 @@
       return this.$store.getters.getCoins;
       }
   },
+  methods: {
+      getColor (change) {
+        if (change > 0) return 'positive'
+        else if (change < 0) return 'negative'
+        
+      },
+    },
   }
   
 </script>
+
+<style scoped>
+.positive{
+  color:green
+}
+.negative{
+  color: red;
+}
+</style>
